2a2a8fea1f863d6251acbf6a062f5e839e4f183f
Plamen Rafailov <prafailov9@gmail.com>
2026-09-01 16:25:42 +0300

    revert working version

--- src/main/java/com/ntros/server/HttpServerWrapper.java
+++ src/main/java/com/ntros/server/HttpServerWrapper.java
@@ -106,16 +106,15 @@ public class HttpServerWrapper implements Server, LifeCycle {
             byte[] responseBytes;
 
             if (filenames.isEmpty()) {
-              String payload = "No Content";
+              String payload = "No files available for download";
               responseBytes = payload.getBytes(StandardCharsets.UTF_8);
 
-              // 204 - successful op, no files exist
-              exchange.sendResponseHeaders(204, responseBytes.length);
+              exchange.sendResponseHeaders(404, responseBytes.length);
               log.info(payload);
             } else {
               String payload = String.join("\n", filenames);
               responseBytes = payload.getBytes(StandardCharsets.UTF_8);
-              // 200 - success, files exist
+
               exchange.sendResponseHeaders(200, responseBytes.length);
             }
             log.info("Listing files");
